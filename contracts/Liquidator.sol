e5a34a2e29a2b5d04c399b4c23c47a2b727f3e74
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-01-17 00:00:46 -0500

    redefining liquidations

--- contracts/Liquidator.sol
+++ contracts/Liquidator.sol
@@ -9,10 +9,19 @@ pragma solidity 0.8.4;
 */
 
 import "contracts/interfaces/IAssetsAccountantState.sol";
+import "contracts/interfaces/IHouseOfCoin.sol";
 import "./interfaces/IHouseOfReserveState.sol";
 import "redstone-evm-connector/lib/contracts/message-based/PriceAware.sol";
 
 contract LiquidatorState {
+
+    /**
+    * @dev Log when a user is in the danger zone of being liquidated.
+    * @param user Address of user that is on margin call. 
+    * @param mintedAsset ERC20 address of user's token debt on margin call.
+    * @param reserveAsset ERC20 address of user's backing collateral.
+    */
+    event MarginCall (address indexed user, adddress indexed mintedAsset, address indexed reserveAsset);
 
     /**
     * @dev Log when a user is liquidated.
@@ -20,34 +29,23 @@ contract LiquidatorState {
     * @param liquidator Address of user that liquidates.
     * @param amount payback.
     */
-    event liquidation(address indexed userLiquidated, adddress indexed liquidator, uint amount);
+    event Liquidation (address indexed userLiquidated, adddress indexed liquidator, uint amount);
 
 }
 
 contract Liquidator is LiquidatorState{
 
     /**
-    * @dev Called to liquidate an array of users.
-    * @param usersToLiquidate Array of the address to liquidate.
+    * @dev Called to liquidate a user.
+    * @param userToLiquidate Array of the address to liquidate.
+    * @param houseOfReserve the house of reserve where user will be liquidated. 
     */
-    function liquidateUsers(address[] calldata usersToLiquidate) external {
+    function liquidateUser(address userToLiquidate, address houseOfReserve) external {
+        // 1.- Check minted power
+        // 2.- Check user minted debt
+        // 3.- Compute health ratio 
 
-    }
 
-    function checkIfLiquidatable(
-        uint reserveBal,
-        uint mintedCoinBal,
-        IHouseOfReserveState.Factor memory collatRatio,
-        uint price
-    ) public pure returns (bool liquidatable, ) {
-
-        uint reserveBalreducedByFactor =
-            ( reserveBal * collatRatio.denominator) / collatRatio.numerator;
-            
-        uint maxMintableAmount =
-            (reserveBalreducedByFactor * price) / 1e8;
-
-        liquidatable = mintedCoinBal > maxMintableAmount? true : false;
     }
 
 }
---
+++ contracts/interfaces/IHouseOfCoin.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+interface IHouseOfCoin  {
+    
+    /**
+    * @notice  Function to mint ERC20 'backedAsset' of this HouseOfCoin.
+    * @dev  Requires user to have reserves for this backed asset at HouseOfReserves.
+    * @param reserveAsset ERC20 address of asset to be used to back the minted coins.
+    * @param houseOfReserve Address of the {HouseOfReserves} contract that manages the 'reserveAsset'.
+    * @param amount To mint. 
+    * Emits a {CoinMinted} event.
+    */
+    function mintCoin(address reserveAsset, address houseOfReserve, uint amount) public;
+
+    /**
+    * @notice  Function to payback ERC20 'backedAsset' of this HouseOfCoin.
+    * @dev Requires knowledge of the reserve asset used to back the minted coins.
+    * @param _backedTokenID Token Id in {AssetsAccountant}, releases the reserve asset used in 'getTokenID'.
+    * @param amount To payback. 
+    * Emits a {CoinPayback} event.
+    */
+    function paybackCoin(uint _backedTokenID, uint amount) public;
+
+    /**
+    * @notice  External function that returns the amount of backed asset coins user can mint with unused reserve asset.
+    * @param user to check minting power.
+    * @param reserveAsset Address of reserve asset.
+    */
+    function checkMintingPower(address user, address reserveAsset) external view returns(uint)
+    
+}
