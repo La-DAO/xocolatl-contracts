431f24cef478821a8281edc36e0123ed74079fc2
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-03-06 21:12:12 -0500

    liquidation script bot phase 1 complete

--- scripts/03_liquidations.js
+++ scripts/03_liquidations.js
@@ -9,10 +9,10 @@ const { removeDuplicates } = require("./helpers.js");
 /**
  * @note Returns an array of addresses that have minted coin in a specific 'HouseOfCoin' deployed contract.
  * @param coinhouse etherjs contract object representing 'HouseOfCoin' deployed contract.
- * @param backedTokenID Id of the token minted.
+ * @param backedTokenID Id of the backed token minted.
  */
 const getHistoricCoinMinters = async function (coinhouse, backedTokenID) {
-  console.log("...Getting historic coin minters");
+  console.log("...getting historic coin minters");
   const path = "./scripts/output/coinminted-events.json";
   if (fs.existsSync(path)) {
     let savedData = JSON.parse(fs.readFileSync(path).toString());
@@ -76,16 +76,80 @@ const fetchCoinMintEvents = async function (coinhouse, startFromBlock = 9913500)
 /**
  * @note Returns an array of validated addresses that continue to have an open debt position in a specific 'HouseOfCoin' deployed contract.
  * @param arrayOfHistoricMinters Array of addresses obtained from 'getHistoricCoinMinters()'.
- * @param coinhouse etherjs contract object representing 'HouseOfCoin' deployed contract.
- * @param reserveAsset address of the reserve asset. 
- * @param assetsaccountant ethersjs contract object representing 'AssetsAccountant' deployed contract.
+ * @param accountant ethersjs contract object representing 'AssetsAccountant' deployed contract.
+ * @param backedTokenID Id of the backed token minted.
  */
-const validateMinters = async function (arrayOfHistoricMinters, coinhouse, reserveAsset, assetsaccountant) {
-  const bid = await coinhouse.getBackedTokenID()
+const validateMinters = async function (arrayOfHistoricMinters, accountant, backedTokenID) {
+  console.log("...validating active minters");
   let validated = [];
   for (let index = 0; index < arrayOfHistoricMinters.length; index++) {
+    let bal = await accountant.balanceOf(arrayOfHistoricMinters[index], backedTokenID);
+    if (bal.gt(0)) {
+      validated.push(arrayOfHistoricMinters[index]);
+    }
+  }
+  return validated;
+}
 
+/**
+ * @note Returns an array of addresses that are liquidatable.
+ * @param addressArray Array of addresses obtained from 'validateMinters()'.
+ * @param coinhouse ethersjs contract object representing 'HouseOfCoin' deployed contract.
+ * @param liquidator etherjs wallet object of liquidator.
+ * @param reserve ethersjs contract of the reserve asset.
+ */
+const checkLiquidatable = async function (addressArray, coinhouse, liquidator, reserve) {
+  console.log("...checking for liquidatable minters");
+  const wrappedcoinhouse = 
+    WrapperBuilder.wrapLite(coinhouse.connect(liquidator)).usingPriceFeed("redstone-stocks");
+  const liqParam = await coinhouse.liqParam();
+  let liquidatable = [];
+  for (let index = 0; index < addressArray.length; index++) {
+    let hr = await wrappedcoinhouse.computeUserHealthRatio(addressArray[index], reserve.address);
+    if (hr.lte(liqParam.liquidationThreshold)) {
+      liquidatable.push(addressArray[index]);
+    }
+  }
+  return liquidatable;
+}
 
+/**
+ * @note Returns the amount of backing asset required to liquidate users.
+ * @param addressArray Array of addresses obtained from 'checkLiquidatable()'.
+ * @param coinhouse ethersjs contract object representing 'HouseOfCoin' deployed contract.
+ * @param liquidator etherjs wallet object of liquidator.
+ * @param reserve ethersjs contract of the reserve asset.
+ */
+const computeLiquidationCost = async function(addressArray, coinhouse, liquidator, reserve) {
+  console.log("...calculating cost of liquidation");
+  const wrappedcoinhouse = 
+    WrapperBuilder.wrapLite(coinhouse.connect(liquidator)).usingPriceFeed("redstone-stocks");
+  let totalCost = ethers.BigNumber.from(0);
+  for (let index = 0; index < addressArray.length; index++) {
+    [
+      costAmount,
+      collateralPenalty 
+    ] = await wrappedcoinhouse.computeCostOfLiquidation(addressArray[index], reserve.address);
+    totalCost = costAmount.add(totalCost);
+  }
+  return totalCost;
+}
+
+/**
+ * @note Execute liquidatio of users.
+ * @param addressArray Array of addresses obtained from 'checkLiquidatable()'.
+ * @param coinhouse ethersjs contract object representing 'HouseOfCoin' deployed contract.
+ * @param liquidator etherjs wallet object of liquidator.
+ * @param reserve ethersjs contract of the reserve asset.
+ */
+const liquidateUsers = async function (addressArray, coinhouse, liquidator, weth) {
+  const wrappedcoinhouse = 
+    WrapperBuilder.wrapLite(coinhouse.connect(liquidator)).usingPriceFeed("redstone-stocks");
+  for (let index = 0; index < addressArray.length; index++) {
+    console.log(`...liquidating user ${addressArray[index]}`);
+    let txLiquidate = await wrappedcoinhouse.liquidateUser(addressArray[index], weth.address);
+    let txResponse = await txLiquidate.wait();
+    console.log(`...succesfully liquidated user ${addressArray[index]} per txHash: ${txResponse.transactionHash }`);
   }
 }
 
@@ -97,20 +161,36 @@ async function main() {
 
   const coinhouse = contracts.coinhouse;
   const reservehouse = contracts.reservehouse;
+  const accountant = contracts.accountant;
+  const weth = contracts.weth;
+  const xoc = contracts.xoc;
 
-  const rid = await reservehouse.reserveTokenID();
   const bid = await reservehouse.backedTokenID();
 
   const historicMinters = await getHistoricCoinMinters(coinhouse, bid);
-  console.log(historicMinters);
+  const validminters = await validateMinters(historicMinters, accountant, bid);
 
-  const wrappedcoinhouse = WrapperBuilder.wrapLite(coinhouse.connect(liquidator)).usingPriceFeed("redstone-stocks");
+  const liquidatableUsers = await checkLiquidatable(validminters, coinhouse, liquidator, weth);
 
-  // [
-  //   costAmount,
-  //   collateralPenalty
-  // ] = await wrappedcoinhouse.computeCostOfLiquidation(dumbUser.address, mockweth.address);
-
+  if (liquidatableUsers.length > 0) {
+    const cost = await computeLiquidationCost(validminters, coinhouse, liquidator, weth);
+    const liquidatorBal = await xoc.balanceOf(liquidator.address);
+    if (liquidatorBal.gte(cost)) {
+      try {
+        console.log("...approving funds for liquidation");
+        const txApproval = await xoc.connect(liquidator).approve(coinhouse.address, cost);
+        await txApproval.wait();
+        await liquidateUsers(liquidatableUsers, coinhouse, liquidator, weth);
+      } catch (error) {
+        console.log("Error!")
+        console.log(error);
+      }
+    } else {
+      console.log("liquidator with not sufficient balance for liquidation");
+    } 
+  } else {
+    console.log("No users to liquidate.");
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
