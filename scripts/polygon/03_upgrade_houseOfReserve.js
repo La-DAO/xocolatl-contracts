e1597a29d8119f7d4efd1f51aa1a4c45b8b7aad0
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-01-18 22:03:38 -0700

    chore: upgrade script house of reserve polygon

---
+++ scripts/polygon/03_upgrade_houseOfReserve.js
@@ -0,0 +1,35 @@
+const { 
+  network,
+  setDeploymentsPath,
+  setPublishPath, 
+  publishUpdates
+} = require("../utils");
+
+const { VERSION } = require("./utils_polygon");
+
+const { upgradeHouseOfReserve } = require("../tasks/upgradeHouseOfReserve");
+
+const upgradeHouseOfReserve = async () => {
+  console.log("\n\n 📡 UpgradingHouseOfReserve...\n");
+  const implementationAddr = await upgradeHouseOfReserve(
+    'HouseOfReserveWETH'
+  );
+  console.log("New implementation address:", implementationAddr);
+}
+
+const main = async () => {
+  if (network !== "polygon") {
+    throw new Error("Set 'NETWORK=polygon' in .env file");
+  }
+  await setDeploymentsPath(VERSION);
+  await setPublishPath(VERSION);
+  await upgradeHouseOfReserve();
+  await publishUpdates();
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(`\n${error}\n`);
+    process.exit(1);
+  });
---
+++ scripts/tasks/upgradeHouseOfReserve.js
@@ -0,0 +1,35 @@
+const {
+  getContract,
+  updateDeployments
+} = require("../utils");
+
+const { ethers, upgrades } = hre;
+
+const upgradeHouseOfReserve = async (
+  detailName
+) => {
+  const contractName = "HouseOfReserve";
+  const reservehouse = await getContract(contractName);
+  console.log(detailName, reservehouse.address);
+  const contractArtifact = await ethers.getContractFactory(contractName);
+  const proxyOpts = {
+    timeout: 600000,
+    kind: 'uups'
+  };
+  await upgrades.validateUpgrade(
+    reservehouse.address,
+    contractArtifact,
+    proxyOpts
+  );
+  const implementation = await upgrades.prepareUpgrade(
+    reservehouse.address,
+    contractArtifact,
+    proxyOpts,
+  );
+  await updateDeployments(detailName, contractName, reservehouse.address);
+  return implementation;
+};
+
+module.exports = {
+  upgradeHouseOfReserve,
+};
