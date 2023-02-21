8deccf6e0ca8a58e3a7b47fb98e92c157d317a75
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-02-21 15:29:40 -0700

    chore: update scripts for goerli deployment

---
+++ scripts/tasks/upgradeHouseOfCoin.js
@@ -0,0 +1,33 @@
+const {
+  getContract,
+  updateDeployments
+} = require("../utils");
+
+const { ethers, upgrades } = hre;
+
+const upgradeHouseOfCoin = async () => {
+  const contractName = "HouseOfCoin";
+  const coinhouse = await getContract(contractName, contractName);
+  console.log(contractName, coinhouse.address);
+  const contractArtifact = await ethers.getContractFactory(contractName);
+  const proxyOpts = {
+    timeout: 600000,
+    kind: 'uups'
+  };
+  await upgrades.validateUpgrade(
+    coinhouse.address,
+    contractArtifact,
+    proxyOpts
+  );
+  const implementation = await upgrades.prepareUpgrade(
+    coinhouse.address,
+    contractArtifact,
+    proxyOpts,
+  );
+  await updateDeployments(detailName, contractName, coinhouse.address);
+  return implementation;
+};
+
+module.exports = {
+  upgradeHouseOfCoin,
+};
