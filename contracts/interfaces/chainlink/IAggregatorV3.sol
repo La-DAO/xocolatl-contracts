06f03c7ded55b5621a54e0ad363c62976f71f3d1
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-05-19 00:02:59 +0100

    chore: add missing methods in chainlink interface

--- contracts/interfaces/chainlink/IAggregatorV3.sol
+++ contracts/interfaces/chainlink/IAggregatorV3.sol
@@ -8,19 +8,11 @@ interface IAggregatorV3 {
 
     function version() external view returns (uint256);
 
-    // getRoundData and latestRoundData should both raise "No data present"
-    // if they do not have data to report, instead of returning unset values
-    // which could be misinterpreted as actual reported values.
-    function getRoundData(uint80 _roundId)
-        external
-        view
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        );
+    function latestAnswer() external view returns (int256);
+
+    function latestTimestamp() external view returns (uint256);
+
+    function latestRound() external view returns (uint256);
 
     function latestRoundData()
         external
