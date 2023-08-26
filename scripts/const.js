59b42ef231594beb59050edc154bcc4bd9b6602c
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-08-26 06:36:54 -0700

    chore: create scripts and add const for sepolia and mumbai

--- scripts/const.js
+++ scripts/const.js
@@ -126,6 +126,40 @@ const ASSETS = {
       balanceOf: 0,
     }
   },
+  sepolia: {
+    weth: {
+      address: "0xD0dF82dE051244f04BfF3A8bB1f62E1cD39eED92",
+      storageSlots: {
+        balanceOf: 3,
+      },
+    },
+    wbtc:{
+      address: "0xf864F011C5A97fD8Da79baEd78ba77b47112935a"
+    },
+    storageSlots: {
+      balanceOf: 0,
+    }
+  },
+  mumbai: {
+    wmatic: {
+      address: "0xf237dE5664D3c2D2545684E76fef02A3A58A364c",
+      storageSlots: {
+        balanceOf: 3,
+      },
+    },
+    weth: {
+      address: "0xD087ff96281dcf722AEa82aCA57E8545EA9e6C96",
+      storageSlots: {
+        balanceOf: 0,
+      },
+    },
+    wbtc:{
+      address: "0x97e8dE167322a3bCA28E8A49BC46F6Ce128FEC68"
+    },
+    storageSlots: {
+      balanceOf: 0,
+    }
+  },
 }
 
 const UMA_CONTRACTS = {
