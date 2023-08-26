25801bf132f308b1d18c6f9d2487e76ad11dae1b
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-08-26 06:36:17 -0700

    chore: add sepolia and mumbai configs to hardhat configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -145,6 +145,18 @@ module.exports = {
         [process.env.PRIVATE_KEY] :
         { mnemonic: mnemonic() },
     },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: process.env.PRIVATE_KEY ?
+        [process.env.PRIVATE_KEY] :
+        { mnemonic: mnemonic() },
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: process.env.PRIVATE_KEY ?
+        [process.env.PRIVATE_KEY] :
+        { mnemonic: mnemonic() },
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
