06182e7753d44335e4e77ff9f46891ef3cf2fe4c
DCota <32775237+DaigaroCota@users.noreply.github.com>
2024-04-26 21:43:54 +0100

    refactor setup assets accountant

--- scripts/tasks/setUpAssetsAccountant.js
+++ scripts/tasks/setUpAssetsAccountant.js
@@ -1,17 +1,15 @@
-const {ethers} = require("hardhat");
-
-const setUpAssetsAccountant = async (contract, houseOfCoinAddr, reserveFactoryAddr, liquidatorAddr) => {
+const setUpAssetsAccountant = async (contract, houseOfCoinAddr, liquidatorAddr, reserveFactoryAddr) => {
     const stx1 = await contract.registerHouse(houseOfCoinAddr);
     await stx1.wait();
     console.log("...House of Coin registered in AssetsAccountant");
 
-    const stx2 = await contract.registerHouse(houseOfReserveAddr);
+    const stx2 = await contract.allowLiquidator(liquidatorAddr, true);
     await stx2.wait();
-    console.log("...House of Reserve registered in AssetsAccountant");
-
-    const stx3 = await contract.allowLiquidator(liquidatorAddr, true);
-    await stx3.wait();
     console.log("...liquidator set in AssetsAccountant");
+
+    const stx3 = await contract.allowReserveFactory(reserveFactoryAddr);
+    await stx3.wait();
+    console.log("...House of Reserve Factory allowed in AssetsAccountant");
 };
 
 module.exports = {
