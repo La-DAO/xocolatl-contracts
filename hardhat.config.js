f157340a30ceafaebfb4c0b1484772603e4f06fb
DCota <32775237+DaigaroCota@users.noreply.github.com>
2024-04-27 04:11:34 -0700

    chore updagrade hardhat to latest

--- hardhat.config.js
+++ hardhat.config.js
@@ -49,50 +49,57 @@ module.exports = {
       */
         },
         mainnet: {
+            chainId: 1,
             url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         polygon: {
+            chainId: 137,
             url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_ID}`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         polygonzkevm: {
+            chainId: 1101,
             url: `https://zkevm-rpc.com`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         arbitrum: {
+            chainId: 42161,
             url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_ID}`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         optimism: {
+            chainId: 10,
             url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_ID}`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         gnosis: {
+            chainId: 100,
             url: `https://rpc.ankr.com/gnosis`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         binance: {
+            chainId: 56,
             url: `https://bsc-dataseed1.binance.org`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         linea: {
+            chainId: 59144,
             url: `https://linea-mainnet.infura.io/v3/${process.env.INFURA_ID}`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         base: {
+            chainId: 8453,
             url: `https://base-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
-        goerli: {
-            url: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`,
-            accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
-        },
         sepolia: {
+            chainId: 11155111,
             url: `https://sepolia.infura.io/v3/${process.env.INFURA_ID}`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
         mumbai: {
+            chainId: 80001,
             url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_ID}`,
             accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {mnemonic: mnemonic()},
         },
